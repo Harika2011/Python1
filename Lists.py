34b3b6f8d6597d0d9c9a537dc1576e09b654854e
Harika <103110286+Harika2011@users.noreply.github.com>
2025-01-07 09:52:24 +0530

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,12 @@
+empty_list = []
+
+elements_list = [1,2,3,4,5]
+
+replicated_list = elements_list * 2
+
+reversed_list = elements_list[::-1] 
+
+print("Empty List:", empty_list)
+print("Original List:", elements_list)
+print("Replicated List:", replicated_list)
+print("Reversed List:", reversed_list)
