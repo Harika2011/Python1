4d103398e3f5630284ae212b496621e84032b100
Harika <103110286+Harika2011@users.noreply.github.com>
2025-01-17 09:32:26 +0530

    Create set_operations.py

---
+++ set_operations.py
@@ -0,0 +1,16 @@
+my_set = {1,2,3,4}
+print("Set with integers",my_set)
+
+mixed_set = {1,"Hello",11.12,(1,2,3,4)}
+print("Set that includes mixed elements ",mixed_set)
+
+duplicates_set = {1,2,3,4,5,3,2}
+print("set after removing duplicates ",duplicates_set)
+
+list_set = set([1,2,3,4])
+print("Set created from a list ",list_set)
+
+modify_set = {0, 1, 3, 4, 5}
+modify_set.remove(0)
+print("Set after removing the element 0 ",modify_set)
+
