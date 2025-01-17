d1988a10de00d281a4cb3c4c140f1149745e91f4
Harika <103110286+Harika2011@users.noreply.github.com>
2025-01-17 09:35:36 +0530

    Create set_intersection.py

---
+++ set_intersection.py
@@ -0,0 +1,9 @@
+#Write a program to find the intersection of two sets. Set1 = {green, blue} Set2 = {blue, yellow}
+
+
+set1 = {"green","blue"}
+set2 = {"blue","yellow"}
+
+intersection = set1.intersection(set2)
+
+print(intersection)
