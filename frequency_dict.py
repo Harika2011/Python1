5854a0437fa3765e17f855d5036d66546dd3475a
Harika <103110286+Harika2011@users.noreply.github.com>
2025-01-20 09:54:57 +0530

    Create frequency_dict.py

---
+++ frequency_dict.py
@@ -0,0 +1,13 @@
+test_dict = {
+    "a": 1,
+    "b": 2,
+    "c": 1,
+    "d": 3,
+    "e": 1
+}
+
+value_to_check = 1
+
+frequency = list(test_dict.values()).count(value_to_check)
+
+print(f"The frequency of {value_to_check} is: {frequency}")
