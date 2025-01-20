9ad353d92afb6ebe5b06e25733a090de7732316b
Harika <103110286+Harika2011@users.noreply.github.com>
2025-01-20 09:48:34 +0530

    Create list_comprehension.py

---
+++ list_comprehension.py
@@ -0,0 +1,4 @@
+#Perform List Comprehension to get mentioned results.
+
+result = [x * 200 for x in [1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,18,19,20,21,22,23,24,25,26,28,29,30,31,32,33,34,35,36,37,39,40]]
+print(result)
