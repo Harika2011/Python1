f67224e9dd32437ac939a8ad5661912160cc06d8
Harika <103110286+Harika2011@users.noreply.github.com>
2025-01-20 09:52:48 +0530

    Create set_hw.py

---
+++ set_hw.py
@@ -0,0 +1,12 @@
+#Write a Python program to find the symmetric difference between two sets.
+
+
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+symmetric_difference = set1.symmetric_difference(set2)
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("Symmetric Difference:", symmetric_difference)
