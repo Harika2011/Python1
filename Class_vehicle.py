998efb0ba9b2371fa359dc15971d4da985a22917
Harika <103110286+Harika2011@users.noreply.github.com>
2025-01-24 09:25:13 +0530

    Create Class_vehicle.py

---
+++ Class_vehicle.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def __init__(self,max_speed,mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+car = Vehicle(240,18)
+
+print("Maximum Speed :",car.max_speed)
+print("Mileage : ",car.mileage)
+        
