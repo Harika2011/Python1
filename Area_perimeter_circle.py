920ab4c19e49227d2684f44c29dc22169ec73f37
Harika <103110286+Harika2011@users.noreply.github.com>
2025-01-27 09:27:55 +0530

    Create Area_perimeter_circle.py

---
+++ Area_perimeter_circle.py
@@ -0,0 +1,27 @@
+#Write a Python program to create a class named Circle constructed by a radius and two methods to compute the area and the perimeter of a circle.
+
+import math
+
+class Circle:
+    def __init__(self, radius):
+       
+        self.radius = radius
+
+    def compute_area(self):
+        
+        return math.pi * self.radius ** 2
+
+    def compute_perimeter(self):
+        
+        return 2 * math.pi * self.radius
+
+def main():
+    radius = float(input("Enter the radius of the circle: "))
+    
+    circle = Circle(radius)
+    
+    print(f"Area of the circle: {circle.compute_area():.2f}")
+    print(f"Perimeter of the circle: {circle.compute_perimeter():.2f}")
+
+
+main()
