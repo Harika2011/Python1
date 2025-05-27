da6d86fe66d9843e762e47f03e96fea7c4316fff
Harika <103110286+Harika2011@users.noreply.github.com>
2025-05-27 12:05:35 +0530

    Create ImplementBitwise.py

---
+++ ImplementBitwise.py
@@ -0,0 +1,22 @@
+num1 = int(input("Enter a number between 1 and 10"))
+num2 = int(input("Enter another number between 1 and 10"))
+
+#AND
+print("num1 & num2 = ",num1&num2)
+
+#OR
+print("\n num1| num2 = ",num1|num2)
+
+#NOT
+print("\n~num1 =",~num1)
+
+#XOR
+print("\n num1 ^ num2 = ",num1 ^ num2)
+
+#RIGHT SHIFT
+print("\n num1 >> 1 = ",num1<<1)
+print("\n num2 >> 1 = ",num2 >> 1)
+
+#LEFT SHIFT
+print("\n num1 << 1 = ",num1 << 1)
+print("\n num2 << 1 = ",num2 << 1)
